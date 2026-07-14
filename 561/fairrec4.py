720b5d8c8aad3007a580fdaec6b929340bdd2242
antony-561 <imca-561@scmsgroup.org>
2026-07-14 10:26:59 +0530

    Add files via upload

---
+++ 561/fairrec4.py
@@ -0,0 +1,31 @@
+ch = 0
+while ch != 9:
+    ch = int(input("Enter Choice"))
+    match ch :
+        case 1:
+            print("Right Traingle")
+            for i in range(10):
+                print("*" * i)
+        case 2:
+            print("Inverted Right Triangle")
+            for i in range(10,0,-1):
+                print("*" * i)
+        case 3:
+            print("Number Triangle")
+            for i in range(1,10+1):
+                print()
+                for k in range(1,i+1):
+                        print(k ," ",end = "")
+            print()
+        case 4:
+            print("Repeated number pattern")
+            for i in range(10):
+                print(str(i) * i)
+        case 5:
+            print("Floyds Triangle")
+            for i in range(1,10+1):
+                for k in range(1,i):
+                    print(i)
+                print()
+            
+        
