5d7ea4a14713f0f971e476900a9f23697472f24e
antony-561 <imca-561@scmsgroup.org>
2026-08-13 09:00:17 +0530

    q14 almost done

--- 561/fairrec14.py
+++ 561/fairrec14.py
@@ -23,4 +23,15 @@ ch = 1
 
 while(ch!=5):
     ch = int(input("enter ch:"))
+    match ch:
+        case 1:
+            ins()
+        case 2:
+            delete()
+        case 3:
+            peek()
+        case 4:
+            isEmpty()
+        case 5:
+            print("Exit")
     
