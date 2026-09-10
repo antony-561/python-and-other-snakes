c4f429e5493ca22a3dcde3b8166fe8f88edb7ac6
antony-561 <imca-561@scmsgroup.org>
2026-09-10 12:40:22 +0530

    Add files via upload

---
+++ kinterfiles.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+root.title("HElloWorld")
+root.geometry("1280x600")
+
+label = Label(root, text = "HEEEEEEEEEE")
+label.pack()
+
+button = Button(root, text = "CLICK HERE")
+button.pack()
+
+
+root.mainloop()
